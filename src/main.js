515e20d6cb80b77256f70e9946d1a6f2d996ea7d
nutmmm <nutmmm@organicdesign.co.nz>
2018-11-10 11:24:59 +1300

    Init project with plugins.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import './registerServiceWorker'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
